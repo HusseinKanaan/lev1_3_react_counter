9aba05e0d8f1e39f3c417be5e0a534acd4561cdb
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-09-01 22:09:57 +0200

    change CD

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Counter from './components/Counter.jsx'
 import './App.css';
 import Expander from './components/Expander.js';
+import Expanders from './components/Expanders';
 
 
 function App() {
   return (
     <div className="App">
       {/* <Counter></Counter> */}
-      <Expander/>
+      {/* <Expander/> */}
+      <Expanders/>
     </div>
   );
 }
