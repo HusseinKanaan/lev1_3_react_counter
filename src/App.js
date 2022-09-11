03adc011421cb466be58dce35a95208a729c5500
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-09-11 20:30:13 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Counter from './components/Counter.jsx'
 import './App.css';
-import Expander from './components/Expander.js';
-import Expanders from './components/Expanders';
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter></Counter> */}
-      {/* <Expander/> */}
-      <Expanders/>
+      <Counter></Counter> 
+
     </div>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,6 +6,7 @@ const Counter = () => {
 
     /*  State hook */
     const [count, setCount] = useState(0)
+    const [reset, setReset] = useState(false)
 
     /* Funktion */
     const add = () => {
@@ -14,10 +15,11 @@ const Counter = () => {
     /* Funktion */
     const minus = () => {
         // Das ist der ternary operator eine kurzschreibweise von if else
-        setCount !== 0 ? setCount(count - 1) : setCount(0)
+        setCount !== 0 ? setCount(count - 1) : setCount(count)
     }
-    const reset = () => {
-        setCount !== setCount ? setCount(count(0)) : setCount(0)
+    const resetbtn = () => {
+        setReset(!reset)
+        !reset ? setCount(count) : setCount(0)
     }
     return (
         <div className='flexBox'>
@@ -27,7 +29,7 @@ const Counter = () => {
                 <button><span>{count}</span></button>
                 <button onClick={minus}>-</button>
             </div>
-            <button className='resetButton' onClick={reset}>reset</button>
+            <button className='resetButton' onClick={resetbtn}>reset</button>
         </div>
 
 
